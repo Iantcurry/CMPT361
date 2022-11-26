58c7558b09d769f503431950ec4c3adf391f9603
SageJu <51926688+SageJu@users.noreply.github.com>
2022-11-25 20:31:29 -0700

    Added Early Stage viewEmail function
    Haven't tested yet, will test soon.  Just wanted to get my thoughts to paper.

--- Client.py
+++ Client.py
@@ -1,98 +1,51 @@
-# Sera Vallee
-# 3045024
-# CMPT 361
-# L7
-
 import socket
-import sys
 import os
+import sys
+import random
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-def getKey():
-    f = open("key", 'rb')
-    key = f.read()
-    f.close()
-
-    return key
-
-def encrypt(message):
-    key = getKey()
-    raw = pad(message.encode(), 32)
-    cipher = AES.new(key, AES.MODE_ECB)
-    encryptedMsg = cipher.encrypt(raw)
-
-    return encryptedMsg
-
-def decrypt(message):
-    key = getKey()
-    cipher = AES.new(key, AES.MODE_ECB)
-    raw = cipher.decrypt(message)
-    decryptedMsg = unpad(raw, 32).decode('ascii')
-
-    return decryptedMsg
-
-def exam(clientSocket):
-    for i in range(4):
-        # get question
-        question_e = clientSocket.recv(32)
-        question = decrypt(question_e)
-        print(question)
-
-        # send user entry
-        answer = input()
-        answer_e = encrypt(answer)
-        clientSocket.send(answer_e)
-
-
+def viewEmail():
+    message = clientSocket.recv(2048)
+    # decrpyt #
+    
+    index =  input("Enter the email index you wish to view: ")
+    # encrypt #
+    clientSocket.send(index)
+    
+    # recieve email to view from server or message if index was out of range
+    email = clientSocket.recv(2048)
+    # decrypt #
+    
+    # prints email or message if index was out of range
+    print(email)
+    return
 
 def client():
-    """Client: manages connection with server."""
     # Server Information
-    serverName = input("Enter hostname or IP: ")
-    serverPort = 13000
-
-    # Create client socket that useing IPv4 and TCP protocols
+    #serverName = 'cc5-212-19.macewan.ca'#'127.0.0.1' #'localhost'
+    serverName = input("Enter the server IP or name: ")
+    serverPort = 13000 
+    
+    #Create client socket that useing IPv4 and TCP protocols 
     try:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     except socket.error as e:
-        print('Error in client socket creation:', e)
-        sys.exit(1)
-
+        print('Error in client socket creation:',e)
+        sys.exit(1)    
+    
     try:
+        #Client connect with the server
+        clientSocket.connect((serverName,serverPort))
+        viewEmail()
 
-        # Client connect with the server
-        clientSocket.connect((serverName, serverPort))
-        while True:
-            # get first message
-            message1_e = clientSocket.recv(32)
-            message1 = decrypt(message1_e)
-            print(message1)
-
-            message2_e = clientSocket.recv(32)
-            message2 = decrypt(message2_e)
-            flag = message2[0]
-            print(message2[1:])
-
-            # send user entry
-            entry = input()
-            entry_e = encrypt(entry)
-            clientSocket.send(entry_e)
-
-            if flag == '2' and entry != 'y':
-                break
-            else:
-                exam(clientSocket)
-
-        # Client terminate connection with the server
+        # Client is done with server break connection
         clientSocket.close()
-
+        
     except socket.error as e:
-        print('An error occured:', e)
+        print('An error occured:',e)
         clientSocket.close()
         sys.exit(1)
 
-
-# ----------
+#----------
 client()
--- Server.py
+++ Server.py
@@ -1,195 +1,104 @@
-# Sera Vallee
-# 3045024
-# CMPT 361
-# L7
-
 import socket
-import sys
 import os
+import sys
 import random
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-def getKey():
-    f = open("key", 'rb')
-    key = f.read()
-    f.close()
-
-    return key
-
-def encrypt(message):
-    key = getKey()
-    raw = pad(message.encode(), 32)
-    cipher = AES.new(key, AES.MODE_ECB)
-    encryptedMsg = cipher.encrypt(raw)
-
-    return encryptedMsg
-
-def decrypt(message):
-    key = getKey()
-    cipher = AES.new(key, AES.MODE_ECB)
-    raw = cipher.decrypt(message)
-    decryptedMsg = unpad(raw, 32).decode('ascii')
-
-    return decryptedMsg
-
-def printEncMsg(encMsg, name=''):
-    if name != '':
-        print("Encrypted message received from " + name + ": " + str(encMsg))
-    else:
-        print("Encrypted message received: " + str(encMsg))
-
-def printDencMsg(decMsg, name=''):
-    if name != '':
-        print("Decrypted message received from " + name + ": " + decMsg)
-    else:
-        print("Decrypted message received: " + decMsg)
-
-def randomQuestion(num):
-    answer = 0
-    string = ''
-    x = random.randint(0,100)
-    y = random.randint(0,100)
-    op = random.randint(1,3)
-    if op == 1:                 # + add
-        answer = x + y
-        string = f'{"Question"}{num}{": "}{x}{" + "}{y}{" ="}'
-    elif op == 2:               # - sub
-        answer = x - y
-        string = f'{"Question"}{num}{": "}{x}{" - "}{y}{" ="}'
-    elif op == 3:               # * mult
-        answer = x * y
-        string = f'{"Question"}{num}{": "}{x}{" * "}{y}{" ="}'
-
-    return answer, string
-
-
-# Menu option 2
-def exam(connectionSocket, name):
-    score = 0
-    for i in range(1,5):
-        qAns, qStr = randomQuestion(i)
-
-        qStr_e = encrypt(qStr)
-        connectionSocket.send(qStr_e)
-
-        # Receive file and save it
-        ansEntry_e = connectionSocket.recv(32)
-        printEncMsg(ansEntry_e, name)
-        ansEntry = decrypt(ansEntry_e)
-        printDencMsg(ansEntry, name)
-
-        if int(ansEntry) == qAns:
-            score = score + 1
-
-    return score
-
-
-def menu(connectionSocket):
-    """Menu: deals with the menu of the application."""
-
-    # Ask for a name and then receive it
-    examMsg = "Welcome to examination System"
-    examMsg_e = encrypt(examMsg)
-    connectionSocket.send(examMsg_e)
-
-    nameMsg = '1Enter the name: '
-    nameMsg_e = encrypt(nameMsg)
-    connectionSocket.send(nameMsg_e)
-
-    nameEntry_e = connectionSocket.recv(32)
-    printEncMsg(nameMsg_e)
-    nameEntry = decrypt(nameEntry_e)
-    printDencMsg(nameEntry)
-
-    while True:
-        score = exam(connectionSocket, nameEntry)
-
-        # Report score
-        scoreMsg = "You achieved a score of " + str(score) + "/4"
-        scoreMsg_e = encrypt(scoreMsg)
-        connectionSocket.send(scoreMsg_e)
-
-        retryMsg = '2Try again? (y/n)'
-        retryMsg_e = encrypt(retryMsg)
-        connectionSocket.send(retryMsg_e)
-
-        # Get menu select from client
-        menuEntry_e = connectionSocket.recv(32)
-        printEncMsg(menuEntry_e, nameEntry)
-        menuEntry = decrypt(menuEntry_e)
-        printDencMsg(menuEntry, nameEntry)
-
-        # Handle menu options
-        if menuEntry == "y":
-            continue
-
-        else:
-            # Break loop, exit menu, server will handle connection closing
-            break
-
+def viewEmail(clientName):
+    message = "the server request email index"
+    # encrypt #
+    connectionSocket.send(message)
+    
+    # Recieve index from client
+    index = connectionSocket.recv(2048)
+    # decrypt #
+    
+    # must build filePath as it's unique per client
+    filePath = "\\" + clientName + "\\list.txt" 
+    with open(filePath, "r") as file:
+        listE = file.read() # Read list of emails 
+    print(listE)
+    
+    if index < len(listE) - 1: # ensures index in range so it doesn't crash
+        filepath = "" #(clears incase old filepath is really long and ensures we completely overwrite it)
+        filePath = "\\" + clientName + "\\" + listE[index]
+        with open(filePath, "r") as file:
+            email = file.read() # Reads email
+        # encrypt email
+        connectionSocket.send(email)
+    else: 
+        # if index not in range let user know
+        message = "Index out of range, there is no email with that index"
+        # encrypt #
+        connectionSocket.send(message)
+   
+    return
 
 def server():
-    """Server: Manages server application."""
-
     # Server port
     serverPort = 13000
-
-    # Create server socket that uses IPv4 and TCP protocols
+    
+    # Create server socket that uses IPv4 and TCP protocols 
     try:
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
-        #print('Error in server socket creation:', e)
+        print('Error in server socket creation:',e)
         sys.exit(1)
-
+    
     # Associate 12000 port number to the server socket
     try:
         serverSocket.bind(('', serverPort))
     except socket.error as e:
-        #print('Error in server socket binding:', e)
-        serverSocket.close()
-        sys.exit(1)
-
+        print('Error in server socket binding:',e)
+        sys.exit(1)        
+        
     print('The server is ready to accept connections')
-
-    concurrent = 0
-    while True:
-        # The server can only have 1 connection in its queue waiting for acceptance
-        serverSocket.listen(5)
-
+        
+    # The server can only have one connection in its queue waiting for acceptance
+    serverSocket.listen(5)
+        
+    while 1:
         try:
-            if concurrent > 5:
-                os.wait()
-                concurrent -= 1
             # Server accepts client connection
-            connectionSocket, addr = serverSocket.accept()
-            c_pid = os.fork()
-            if c_pid == 0:
-                # Run Menu that the User interacts with
-                menu(connectionSocket)
-
-                # Server terminates client connection
+            connectionSocket, addr = serverSocket.accept()        
+            pid = os.fork()
+            
+            # If it is a client process
+            if  pid == 0:               
+                serverSocket.close() 
+                viewEmail('client1')
+                
+                # Client is done with server break connection
                 connectionSocket.close()
-                break
-            else:
-                concurrent += 1
-                continue
-
+                
+                return
+            
+            # Parent doesn't need this connection
+            connectionSocket.close()
+            
         except socket.error as e:
-            print('A client socket error occurred: ', e)
-            continue
-
-        except KeyboardInterrupt as e:
-            serverSocket.close()
+            print('An error occured:',e)
+            serverSocket.close() 
+            sys.exit(1)        
+        except:
+            print('Goodbye')
+            serverSocket.close() 
             sys.exit(0)
-
-        # except Exception as e:
-        #     trace = e.__traceback__.tb_lineno
-        #     print('Non-Socket exception occurred: ', e)
-        #     print("In: ", trace)
-            # serverSocket.close()
-            # sys.exit(0)
-
-
-# -------
+            
+        
+#-------
 server()
+
+
+
+
+''' For my own notes to keep strings nicely space out
+# Starts compiling results message
+message = "\n\nName \t\t Size(Bytes) \t\t Upload Date and Time\n"
+
+# # Constructs string till all keys cycled and sends
+for k in keys:
+    message += "%-16s %-23s %-5s\n"%(k,str(d[k]["size"]),str(d[k]["time"]))
+connectionSocket.send(message.encode('ascii'))
+'''
