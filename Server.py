43a45f2cdd64dbeda8ecc20592ae37c8b8b31c59
PenutButer <117711650+PenutButer@users.noreply.github.com>
2022-12-01 16:03:06 -0700

    Fixed emails lambda sort

--- Server.py
+++ Server.py
@@ -61,7 +61,7 @@ def getInbox(clientUsername):
                            emailRead[3]])
     
     # Sorts emails by time and date sent
-    emails.sort(key=lambda time: emails[1])
+    emails.sort(key=lambda email: email[1])
 
     # Add index to each email
     for i in range(len(emails)):
